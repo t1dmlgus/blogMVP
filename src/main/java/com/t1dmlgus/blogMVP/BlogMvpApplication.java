bd4e6255d8f836834bbe26fdf978e56a2f8daa06
이의인 <leeuiin@iuiin-ui-MacBookAir.local>
2022-06-20 17:41:40 +0900

    Initial commit

---
+++ src/main/java/com/t1dmlgus/blogMVP/BlogMvpApplication.java
@@ -0,0 +1,15 @@
+package com.t1dmlgus.blogMVP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogMvpApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/com/t1dmlgus/blogMVP/BlogMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t1dmlgus.blogMVP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
